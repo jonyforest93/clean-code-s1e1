b97f0d537708d55d17c17b498883c0752a023d08
jonyforest93 <jony.forest93@gmail.com>
2024-01-03 21:03:39 +0300

    refactor: update HTML/CSS/JS according to rule 3.4 in html-and-css.md

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ var createNewTaskElement=function(taskString){
 
     deleteButton.className="delete-btn btn";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className = "delete-btn__image"
     deleteButton.appendChild(deleteButtonImg);
 
 
